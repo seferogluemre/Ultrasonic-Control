8f337f6de27dde78558c12293a2b5303f671b4a6
seferogluemre <yunusemre5331@icloud.com>
2025-01-11 18:55:28 +0300

    Adjusting the configuration settings of the ultrasonic distance sensor

---
+++ program.cpp
@@ -0,0 +1,15 @@
+int val = 0;
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
